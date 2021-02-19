6a5489eadf61be1dfa77c46941532a752507dba6
Jasper Cheung <23638735+JasperCheung@users.noreply.github.com>
2021-02-18 19:21:28 -0500

    adding top half functions and tests

--- functions.js
+++ functions.js
@@ -1,12 +1,74 @@
 // forEach
 
+Array.prototype.myEach = function(callback) {
+   for(let i = 0; i < this.length; i++){
+    callback(this[i]);
+   }
+  return;
+};
+
+let a = [1,2,3,4,5];
+console.log("myEach");
+a.myEach(b=>console.log(b*2));
+
 // Map
+
+Array.prototype.myMap = function(callback) {
+  let ret = [];
+  for(let i = 0; i < this.length; i++){
+    ret[i] = callback(this[i]);
+  }
+  return ret;
+};
+
+console.log("myMap");
+let aMap = a.myMap((b)=>b * 2);
+console.log(aMap);
 
 // Filter
 
+Array.prototype.myFilter = function(callback) {
+   let ret = [];
+  for(let i = 0; i < this.length; i++){
+    if(callback(this[i])) ret.push(this[i]);
+  }
+  return ret;
+};
+
+console.log("Filter a < 3");
+let aFil = a.myFilter((b)=>b < 3);
+console.log(aFil);
+
 // Some (Any)
 
+Array.prototype.mySome = function(callback) {
+  for(let i = 0; i < this.length; i++){
+    if(callback(this[i])) return true;
+  }
+  return false;
+};
+
+
+console.log("Some a < 5");
+console.log(a.mySome((b)=>   b < 5));
+console.log("Some a > 5");
+console.log(a.mySome((b)=>   b > 5));
+
 // Every
+
+Array.prototype.myEvery = function(callback) {
+  for(let i = 0; i < this.length; i++){
+    if(!callback(this[i])) return false;
+  }
+  return true;
+};
+
+console.log("Every a < 5");
+console.log(a.myEvery((b)=>   b < 5));
+console.log("Every a > 5");
+console.log(a.myEvery((b)=>   b > 5));
+console.log("Every a < 6");
+console.log(a.myEvery((b)=>   b < 6));
 
 // Reduce
 
